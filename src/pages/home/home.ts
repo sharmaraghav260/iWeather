bf9f8fc0f1ceede7d9a39951ee76f46c433ca3ea
sharmaraghav260 <sharmaraghav260@gmail.com>
2017-08-24 11:53:13 -0800

    Api is responsive.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,31 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { WeatherProvider } from '../../providers/weather/weather';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  weather: any;
+  location:{
+    city:string,
+    state:string
+  }
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private weatherProvider: WeatherProvider) {
 
+  }
+
+  ionViewWillEnter(){
+    this.location = {
+      city: 'Boulder',
+      state: 'CO'
+    }
+
+    this.weatherProvider.getWeather(this.location.city, this.location.state).subscribe(weather => {
+      this.weather = weather.current_observation;
+    })
   }
 
 }
